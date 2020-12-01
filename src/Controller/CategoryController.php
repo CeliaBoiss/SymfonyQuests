b2921f56f6a6688ac5fa36bcd6e06039b8c2d75d
CeliaBoiss <boisseau.celia@gmail.com>
2020-12-01 14:20:30 +0100

    symfony quest 8 - correction of a thing

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,8 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Category;
-use App\Entity\Program;
+use App\Repository\CategoryRepository;
+use App\Repository\ProgramRepository;
 
 /**
 * @Route("/categories", name="category_")
@@ -17,9 +17,9 @@ class CategoryController extends AbstractController
      * @Route("/", name="index")
      * @return Response a response instance
      */
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository): Response
     {
-        $categories = $this->getDoctrine()->getRepository(Category::class)->findAll();
+        $categories = $categoryRepository->findAll();
         return $this->render('category/index.html.twig', ['categories' => $categories]);
     }
 
@@ -27,15 +27,15 @@ class CategoryController extends AbstractController
  * @Route("/{categoryName}", name="show")
  * @return Response
  */
-    public function show(string $categoryName): Response
+    public function show(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository): Response
     {
-        $category = $this->getDoctrine()->getRepository(Category::class)->findBy(['name' => $categoryName]);
+        $category = $categoryRepository->findBy(['name' => $categoryName]);
 
         if (!$category) {
             throw $this->createNotFoundException('No category named : ' . $categoryName);
         }
 
-        $programs = $this->getDoctrine()->getRepository(Program::class)->findBy(['category' => $category], ['id' => 'DESC'], 3);
+        $programs = $programRepository->findBy(['category' => $category], ['id' => 'DESC'], 3);
 
         return $this->render('category/show.html.twig', ['programs' => $programs]);
     }
