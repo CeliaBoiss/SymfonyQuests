5bc6eec52b51d8add05fd212d6b5f7aa6db41dc3
CeliaBoiss <boisseau.celia@gmail.com>
2020-12-01 14:28:03 +0100

    another modif in symfony 8

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Program;
+use App\Repository\ProgramRepository;
 
 /**
 * @Route("/programs", name="program_")
@@ -16,9 +16,9 @@ class ProgramController extends AbstractController
      * @Route("/", name="index")
      * @return Response a response instance
      */    
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
-        $programs = $this->getDoctrine()->getRepository(Program::class)->findAll();
+        $programs = $programRepository->findAll();
         return $this->render('program/index.html.twig', ['website' => 'Wild Séries', 'programs' => $programs]);
     }
 
@@ -26,9 +26,9 @@ class ProgramController extends AbstractController
      * @Route("/{id<\d+>}", methods={"GET"}, name="by_id")
      * @return Response a response instance
     */
-    public function show(int $id): Response
+    public function show(int $id, ProgramRepository $programRepository): Response
     {
-        $program = $this->getDoctrine()->getRepository(Program::class)->findOneBy(['id' => $id]);
+        $program = $programRepository->findOneBy(['id' => $id]);
 
         if(!$program) {
             throw $this->createNotFoundException('No program with id : '.$id.' found in program\'s table.');
