36fbd48d5794dc1be14f5a44e205496f769dcd45
CeliaBoiss <boisseau.celia@gmail.com>
2020-12-01 14:49:09 +0100

    another modif in symfony 8

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ProgramRepository;
+use App\Entity\Program;
 
 /**
 * @Route("/programs", name="program_")
@@ -26,10 +27,8 @@ class ProgramController extends AbstractController
      * @Route("/{id<\d+>}", methods={"GET"}, name="by_id")
      * @return Response a response instance
     */
-    public function show(int $id, ProgramRepository $programRepository): Response
+    public function show(Program $program): Response
     {
-        $program = $programRepository->findOneBy(['id' => $id]);
-
         if(!$program) {
             throw $this->createNotFoundException('No program with id : '.$id.' found in program\'s table.');
         }
