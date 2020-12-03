76ad3984af32842544ec9d0f03f9fa65d6401f3f
CeliaBoiss <boisseau.celia@gmail.com>
2020-12-03 11:40:19 +0100

    symfony quest 11

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # wild_series
 
 Quest 9 & 10 : https://youtu.be/lhkcVSWgAM4
+
+Quest 11 : 
+    - add category : https://www.youtube.com/watch?v=q76aV-49H-E&feature=youtu.be
+    - add program : https://www.youtube.com/watch?v=kimod8PSYxs&feature=youtu.be
+    
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CategoryRepository;
 use App\Repository\ProgramRepository;
+use App\Form\CategoryType;
+use App\Entity\Category;
 
 /**
 * @Route("/categories", name="category_")
@@ -23,10 +26,41 @@ class CategoryController extends AbstractController
         return $this->render('category/index.html.twig', ['categories' => $categories]);
     }
 
-/**
- * @Route("/{categoryName}", name="show")
- * @return Response
- */
+    /**
+     * The controller for the category add form
+     *
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request) : Response
+    {
+        // Create a new Category Object
+        $category = new Category();
+        // Create the associated Form
+        $form = $this->createForm(CategoryType::class, $category);
+        // Get data from HTTP request
+        $form->handleRequest($request);
+        // Was the form submitted ?
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // Get the Entity Manager
+            $entityManager = $this->getDoctrine()->getManager();
+            // Persist Category Object
+            $entityManager->persist($category);
+            // Flush the persisted object
+            $entityManager->flush();
+            // Finally redirect to categories list
+            return $this->redirectToRoute('category_index');
+        }
+        // Render the form
+        return $this->render('category/new.html.twig', [
+            "form" => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{categoryName}", name="show")
+     * @return Response
+    */
     public function show(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository): Response
     {
         $category = $categoryRepository->findBy(['name' => $categoryName]);
